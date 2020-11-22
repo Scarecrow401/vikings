cdea8b93b9f007e5ccd6eccbd8e07de9d0c885dd
ScareCrow401 <scarecrow4018@gmail.com>
2020-11-23 02:23:28 +0500

    preview

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
-$(function(){
+$(function () {
+    $('[data-fancybox]').fancybox({
+        youtube: {
+            controls: 0,
+            showinfo: 0
+        }
+    });
 
+    $('.heroes__slider-img').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.heroes__slider-text',
+        prevArrow: '<button class="slick-btn slick-prev"><img src="images/arrow-prev.png" alt="prev"></button>',
+        nextArrow: '<button class="slick-btn slick-next"><img src="images/arrow-next.png" alt="next"></button>'
+    });
+    $('.heroes__slider-text').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.heroes__slider-img',
+    });
 });
